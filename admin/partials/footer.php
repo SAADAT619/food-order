63a13cb888144def3a761a827d9748feb05455f7
SAADAT619 <md.samin.yasar.saadat@g.bracu.ac.bd>
2022-08-24 15:13:26 +0600

    first commit

--- admin/partials/footer.php
+++ admin/partials/footer.php
@@ -1,20 +1,10 @@
-<!--footer section starts--->
-<div class ="footer" >
-       <div class = "wrapper">
-          <p class= "text-center">2022 All right reserved <a href="#">cse370</a> </p> 
- 
-       </div>
-      
-  
- 
-   </div>
-   <!---footer ends--->
- 
- 
- 
-</body>
- 
- 
- 
- 
+<!-- Footer Section Starts -->
+<div class="footer">
+            <div class="wrapper">
+                <p class="text-center">2020 All rights reserved, Food House. Developed By - <a href="#">Samin Yasar Saadat</a></p>
+            </div>
+        </div>
+        <!-- Footer Section Ends -->
+
+    </body>
 </html>
